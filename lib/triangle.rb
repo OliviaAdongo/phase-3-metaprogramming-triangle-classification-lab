67cec084ac853b6bbdcbeee95e3ec93b0ba3b3d5
Olivia Adongo <akinyioliviatove@gmail.com>
2022-08-24 06:35:13 +0300

    commit tree d112141deca42cceca013fc66cb53afc01c8d26e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_a, :side_b, :side_c
+
+  def initialize(length_1, length_2, length_3)
+    @side_a = length_1
+    @side_b = length_2
+    @side_c = length_3
+  end
+
+  def kind
+    ab = @side_a + @side_b
+    bc = @side_b + @side_c
+    ac = @side_a + @side_c
+
+    if(ab > @side_c && bc > @side_a && ac > @side_b && @side_a > 0 && @side_b > 0 && @side_c > 0)
+      if(@side_a == @side_b && @side_a == @side_c)
+        :equilateral
+      elsif(@side_a == @side_b || @side_a == @side_c || @side_b == @side_c)
+        :isosceles
+      else
+        :scalene
+      end
+
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a triangle"
+    end
+  end
+
 end
